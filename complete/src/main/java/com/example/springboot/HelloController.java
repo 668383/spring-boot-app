76351e71c79b8a9bf52ade5923469c5081dc181c
668383 <668383@cognizant.com>
2021-07-20 01:14:06 -0700

    Made Interactive | Added two nums

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,14 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	@RequestMapping("/add")
+	public String index(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+		return "Greetings from Spring Boot!" + (num1 + num2);
 	}
 
 }
